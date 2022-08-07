80b09c082b5169b509f48433de647024dfe087c4
kadenzipfel <kaden.zipfel@hotmail.com>
2022-08-07 16:37:39 -0700

    Cleanup test

--- src/test/MergeMarket.t.sol
+++ src/test/MergeMarket.t.sol
@@ -4,18 +4,10 @@ pragma solidity ^0.8.13;
 import "forge-std/Test.sol";
 import "../MergeMarket.sol";
 
-interface CheatCodes {
-    function prank(address) external;
-    function expectRevert(bytes4) external;
-    function expectRevert(bytes memory) external;
-    function warp(uint256) external;
-}
-
 contract MergeMarketTest is Test {
-    CheatCodes cheats = CheatCodes(HEVM_ADDRESS);
-    MergeMarket mergePredictionMarket;
+    MergeMarket mergeMarket;
 
     function setUp() public {
-        mergePredictionMarket = new MergeMarket();
+        mergeMarket = new MergeMarket();
     }
 }
